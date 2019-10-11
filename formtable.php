54332f070dd1bd623cf889d145d331df003a0586
CalypsoReDecay <56446692+CalypsoReDecay@users.noreply.github.com>
2019-10-11 18:25:26 +0200

    agg table

---
+++ formtable.php
@@ -0,0 +1,12 @@
+<html>
+
+<body>
+
+<form method="get" action="/tablecopy.php?">
+<input type="text" name="colore" placeholder="Colore"><br><br>
+<input type="text" name="num_riga" placeholder="Numero Righe"><br><br>
+<input type="text" name="num_colonne" placeholder="Numero Colonne"><br><br>
+<input type="submit">
+
+</body>
+</html>
