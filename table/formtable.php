05c8b47ae3a12b62be8910e4205f080da110811e
CalypsoReDecay <56446692+CalypsoReDecay@users.noreply.github.com>
2019-10-11 19:30:38 +0200

    agg form post

--- table/formtable.php
+++ table/formtable.php
@@ -2,11 +2,23 @@
 
 <body>
 
-<form method="get" action="/github/programmazione-web/table/tablecopy.php?">
+Form GET<br>
+<form name="formget" method="get" action="/github/programmazione-web/table/tablecopy.php">
 <input type="text" name="colore" placeholder="Colore"><br><br>
 <input type="text" name="num_riga" placeholder="Numero Righe"><br><br>
 <input type="text" name="num_colonne" placeholder="Numero Colonne"><br><br>
 <input type="submit">
+</form>
+
+<br><br>
+
+Form POST<br>
+<form name="formpost" method="post" action="/github/programmazione-web/table/tablecopypost.php">
+<input type="text" name="colore" placeholder="Colore"><br><br>
+<input type="text" name="num_riga" placeholder="Numero Righe"><br><br>
+<input type="text" name="num_colonne" placeholder="Numero Colonne"><br><br>
+<input type="submit">
+</form>
 
 </body>
 </html>
