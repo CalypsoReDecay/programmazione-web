450aacfb59ecd46ee1b42d6dcda3f69c363e9128
CalypsoReDecay <56446692+CalypsoReDecay@users.noreply.github.com>
2019-11-13 19:05:55 +0100

    ese3

--- Esercizio_2/lista_calcoli.php
+++ Esercizio_2/lista_calcoli.php
@@ -7,7 +7,7 @@
 for($x = 1; $x <= 40; $x++){
     $furbetta = $x * 3;
 ?>
-<a href="/github2/Esercizio_2/esegui_calcolo.php?num1=<?=$furbetta?>&num2=<?=$furbetta?>"><?=$furbetta?> * <?=$furbetta?></a><br>
+<a href="/github2/Esercizio_2/esegui_calcolo.php?num1=<?=$furbetta?>&num2=<?=$furbetta?>"><?=$furbetta?>, <?=$furbetta?></a><br>
 <?php
 }
 
---
+++ Esercizio_3/estrazioniLotto.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<?php
+
+$ruota = array (
+    "Bari" => array (25, 30, 50, 60, 90),
+);
+
+foreach ($ruota as $key => $value){
+    print ($key);
+    print_r ($value);
+}
+
+?>
+
+</body>
+</html>
