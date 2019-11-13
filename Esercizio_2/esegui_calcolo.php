50c765d3ebfa2d07b693d76f482bf27937fedca6
CalypsoReDecay <56446692+CalypsoReDecay@users.noreply.github.com>
2019-11-13 18:42:16 +0100

    ese2

---
+++ Esercizio_2/esegui_calcolo.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<?php
+
+print ($_GET['num1'] * $_GET['num2']);
+
+?>
+
+<br><br>
+<a href="/github2/Esercizio_2/lista_calcoli.php"> Indietro </a>
+
+</body>
+</html>
---
+++ Esercizio_2/lista_calcoli.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+
+<?php
+
+
+for($x = 1; $x <= 40; $x++){
+    $furbetta = $x * 3;
+?>
+<a href="/github2/Esercizio_2/esegui_calcolo.php?num1=<?=$furbetta?>&num2=<?=$furbetta?>"><?=$furbetta?> * <?=$furbetta?></a><br>
+<?php
+}
+
+
+
+
+?>
+
+
+
+</body>
+</html>
