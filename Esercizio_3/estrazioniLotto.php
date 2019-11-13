1ab554de88e551f19f39c4a6ce1cce60e7287bd0
CalypsoReDecay <56446692+CalypsoReDecay@users.noreply.github.com>
2019-11-13 19:17:46 +0100

    ese3

--- Esercizio_3/estrazioniLotto.php
+++ Esercizio_3/estrazioniLotto.php
@@ -3,14 +3,32 @@
 
 <?php
 
+function estrazione(){
+    for($x = 1; $x <= 90; $x++){
+        return rand(1, 90) . "<br>";
+
+    }
+}
+
 $ruota = array (
-    "Bari" => array (25, 30, 50, 60, 90),
+    "Bari" => estrazione(),
+    "Cagliari" => estrazione(),
+    "Firenze" => estrazione(),
+    "Genova" => estrazione(),
+    "Milano" => estrazione(),
+    "Napoli" => estrazione(),
+    "Palermo" => estrazione(),
+    "Roma" => estrazione(),
+    "Torino" => estrazione(),
+    "Venezia" => estrazione(),
+    "Nazionale" => estrazione(),
 );
 
 foreach ($ruota as $key => $value){
     print ($key);
     print_r ($value);
 }
+
 
 ?>
 
